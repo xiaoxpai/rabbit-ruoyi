5e8b481d92146fc35b3c6bfb50d4e85fed3cf372
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2023-04-11 14:32:57 +0800

    Update script sql

---
+++ sql/ry_2023-04-11.sql
@@ -0,0 +1,65 @@
+-- ----------------------------
+-- 本地执行SQL，即可实用
+-- 日期：2023年4月11日
+-- ----------------------------
+CREATE TABLE `sys_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `dept_id` bigint(20) DEFAULT NULL COMMENT '部门ID',
+  `login_name` varchar(30) NOT NULL COMMENT '登录账号',
+  `user_name` varchar(30) DEFAULT '' COMMENT '用户昵称',
+  `user_type` varchar(2) DEFAULT '00' COMMENT '用户类型（00系统用户 01注册用户）',
+  `email` varchar(50) DEFAULT '' COMMENT '用户邮箱',
+  `phonenumber` varchar(11) DEFAULT '' COMMENT '手机号码',
+  `sex` char(1) DEFAULT '0' COMMENT '用户性别（0男 1女 2未知）',
+  `avatar` varchar(100) DEFAULT '' COMMENT '头像路径',
+  `password` varchar(50) DEFAULT '' COMMENT '密码',
+  `salt` varchar(20) DEFAULT '' COMMENT '盐加密',
+  `status` char(1) DEFAULT '0' COMMENT '帐号状态（0正常 1停用）',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标志（0代表存在 2代表删除）',
+  `login_ip` varchar(128) DEFAULT '' COMMENT '最后登录IP',
+  `login_date` datetime DEFAULT NULL COMMENT '最后登录时间',
+  `pwd_update_date` datetime DEFAULT NULL COMMENT '密码最后更新时间',
+  `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COMMENT='用户信息表';
+
+-- ----------------------------
+-- 初始化-用户信息表数据
+-- ----------------------------
+insert into sys_user values(1,  103, 'admin', '若依', '00', 'ry@163.com', '15888888888', '1', '', '29c67a30398638269fe600f73a054934', '111111', '0', '0', '127.0.0.1', sysdate(), sysdate(), 'admin', sysdate(), '', null, '管理员');
+insert into sys_user values(2,  105, 'ry',    '若依', '00', 'ry@qq.com',  '15666666666', '1', '', '8e6d98b90472783cc73c17047ddccf36', '222222', '0', '0', '127.0.0.1', sysdate(), sysdate(), 'admin', sysdate(), '', null, '测试员');
+
+
+
+CREATE TABLE `sys_user_online` (
+  `sessionId` varchar(50) NOT NULL DEFAULT '' COMMENT '用户会话id',
+  `login_name` varchar(50) DEFAULT '' COMMENT '登录账号',
+  `dept_name` varchar(50) DEFAULT '' COMMENT '部门名称',
+  `ipaddr` varchar(128) DEFAULT '' COMMENT '登录IP地址',
+  `login_location` varchar(255) DEFAULT '' COMMENT '登录地点',
+  `browser` varchar(50) DEFAULT '' COMMENT '浏览器类型',
+  `os` varchar(50) DEFAULT '' COMMENT '操作系统',
+  `status` varchar(10) DEFAULT '' COMMENT '在线状态on_line在线off_line离线',
+  `start_timestamp` datetime DEFAULT NULL COMMENT 'session创建时间',
+  `last_access_time` datetime DEFAULT NULL COMMENT 'session最后访问时间',
+  `expire_time` int(5) DEFAULT '0' COMMENT '超时时间，单位为分钟',
+  PRIMARY KEY (`sessionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='在线用户记录';
+
+CREATE TABLE `sys_user_post` (
+  `user_id` bigint(20) NOT NULL COMMENT '用户ID',
+  `post_id` bigint(20) NOT NULL COMMENT '岗位ID',
+  PRIMARY KEY (`user_id`,`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户与岗位关联表';
+
+CREATE TABLE `sys_user_role` (
+  `user_id` bigint(20) NOT NULL COMMENT '用户ID',
+  `role_id` bigint(20) NOT NULL COMMENT '角色ID',
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户和角色关联表';
+
+-- 持续更新中
